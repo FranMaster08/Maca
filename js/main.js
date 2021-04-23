729c6036b9014ce96c81466d9fb0f7ca85b68815
Francisco Jimenez <franjicohen@gmail.com>
2021-04-23 12:57:54 -0300

    Se agrega parte responsive para los botones

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ let menu_bar = document.querySelector('#menubar')
 let btnBuscar = document.querySelector('#btnBuscar')
 
 menu_bar.addEventListener('click', () => {
-    
+    if(menu.style.display=='none'){
+        menu.classList.remove('ocultarMenu')
+        menu.classList.add('mostrarMenu')
+    }else{
+        menu.classList.add('ocultarMenu')
+        menu.classList.remove('mostrarMenu')
+    }
 })
 
 btnBuscar.addEventListener('click', () => {
