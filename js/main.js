4c74f74d903dff9a10988e3fd145a96882b0eaec
Francisco Jimenez <franjicohen@gmail.com>
2021-04-23 12:47:47 -0300

    Se agrega parte responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-let menu=document.querySelector('#menu')
-let menu_bar=document.querySelector('#menubar')
+let menu = document.querySelector('#menu')
+let menu_bar = document.querySelector('#menubar')
+let btnBuscar = document.querySelector('#btnBuscar')
 
-menu_bar.addEventListener('click',()=>{
-    menu.classList.toggle('menu-toggle')
+menu_bar.addEventListener('click', () => {
+    
+})
+
+btnBuscar.addEventListener('click', () => {
+    if(buscador.style.display!='block'){
+        document.querySelector('#buscador').style.display = 'block'
+        document.querySelector('#EjecutarBusqueda').style.display = 'block'
+    }else{
+        document.querySelector('#buscador').style.display = 'none'
+        document.querySelector('#EjecutarBusqueda').style.display = 'none'
+    }
 })
